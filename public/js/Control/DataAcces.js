31bea644da557df7d522013a8f5392e2aa583281
BorisFunes <fp20008@ues.edu.sv>
2025-06-02 21:16:51 -0600

    limpia de public

--- public/js/Control/DataAcces.js
+++
@@ -1,6 +0,0 @@
-class DataAccess {
-    constructor() {
-       this.BASE_URL = "http://localhost:9080/PupasSv-1.0-SNAPSHOT/v1/"; //Ruta real
-    }
-}
-export default DataAccess; //Toda la clase se exporta y se consume de manera externa.
